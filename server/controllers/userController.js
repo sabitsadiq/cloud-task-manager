f763170ca3a3a9fbfba0e18d46b2c3dc89e2c2e2
sadiq <sabitsdq@gmail.com>
2024-06-26 11:34:22 +0100

    added the activeUser and deleteUser profile route

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -115,6 +115,81 @@ export const updateUserProfile = async (req, res) => {
     return res.status(400).json({ status: false, message: error.message });
   }
 };
+export const markNotificationRead = async (req, res) => {
+  try {
+    const { userId } = req.user;
+    const { isReadType, id } = req.query;
+    if (isReadType === "all") {
+      await Notice.updateMany(
+        { team: userId, isRead: { $nin: [userId] } },
+        { $push: { isRead: userId } },
+        { new: true }
+      );
+    } else {
+      await Notice.findOneAndUpdate(
+        { _id: id, isRead: { $nin: [userId] } },
+        { $push: { isRead: userId } },
+        { new: true }
+      );
+    }
+    res.status(201).json({ status: true, message: "Done" });
+  } catch (error) {
+    return res.status(400).json({ status: false, message: error.message });
+  }
+};
+export const changeUserPassword = async (req, res) => {
+  try {
+    const { userId } = req.user;
+    const user = await User.findById(userId);
+    if (user) {
+      user.password = req.body.password;
+      await user.save();
+
+      user.password = undefined;
+
+      res
+        .status(201)
+        .json({ status: true, message: "Password changed successfully" });
+    } else {
+      res.status(404).json({ status: false, message: "User not found" });
+    }
+  } catch (error) {
+    return res.status(400).json({ status: false, message: error.message });
+  }
+};
+export const activateUserProfile = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    if (user) {
+      user.isActive = req.body.isActive;
+      await user.save();
+
+      res.status(201).json({
+        status: true,
+        message: `User account has been ${
+          user?.isActive ? "activated" : "disabled"
+        }`,
+      });
+    } else {
+      res.status(404).json({ status: false, message: "User not found" });
+    }
+  } catch (error) {
+    return res.status(400).json({ status: false, message: error.message });
+  }
+};
+export const deleteUserProfile = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await User.findByIdAndDelete(id);
+
+    res
+      .status(200)
+      .json({ status: true, message: "User deleted successfully" });
+  } catch (error) {
+    return res.status(400).json({ status: false, message: error.message });
+  }
+};
 // export const  = async (req, res) => {
 //   try {
 //   } catch (error) {
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,14 @@
 import express from "express";
 import { isAdminRoute, protectedRoute } from "../middlewares/authMiddleware.js";
 import {
+  activateUserProfile,
+  changeUserPassword,
+  deleteUserProfile,
   getNotificationsList,
   getTeamList,
   loginUser,
   logoutUser,
+  markNotificationRead,
   registerUser,
   updateUserProfile,
 } from "../controllers/userController.js";
@@ -19,13 +23,13 @@ router.get("/get-team", protectedRoute, isAdminRoute, getTeamList);
 router.get("/get-notifications", protectedRoute, getNotificationsList);
 
 router.put("/profile", protectedRoute, updateUserProfile);
-// router.put("/read-noti", protectedRoute, markNotificationRead);
-// router.put("/change-password", protectedRoute, changeUserPassword);
+router.put("/read-noti", protectedRoute, markNotificationRead);
+router.put("/change-password", protectedRoute, changeUserPassword);
 
 // // ADMIN ROUTES
-// router
-//   .route("/:id")
-//   .put(protectedRoute, isAdminRoute, activateUserProfile)
-//   .delete(protectedRoute, isAdminRoute, deleteUserProfile);
+router
+  .route("/:id")
+  .put(protectedRoute, isAdminRoute, activateUserProfile)
+  .delete(protectedRoute, isAdminRoute, deleteUserProfile);
 
 export default router;
