4733c75cb188b8d39277511b4d926b979e076219
sadiq <sabitsdq@gmail.com>
2024-07-06 15:19:58 +0100

    updated the task pages

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -45,7 +45,7 @@ const linkData = [
   },
   {
     label: "Trash",
-    link: "trash",
+    link: "trashed",
     icon: <FaTrashAlt />,
   },
 ];
@@ -56,7 +56,8 @@ const Sidebar = () => {
   const dispatch = useDispatch();
   const location = useLocation();
   const path = location.pathname.split("/")[1];
-  const sidebarLinks = user?.isAdmin ? linkData : linkData.slice(0, 5);
+  // const sidebarLinks = user?.isAdmin ? linkData : linkData.slice(0, 5);
+  const sidebarLinks = linkData;
   const closeSidebar = () => {
     dispatch(setOpenSidebar(false));
   };
